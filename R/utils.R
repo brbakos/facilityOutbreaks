9c97321d05d541bd74bd5abbd35cb7d58e6d8405
brbakos <bbakos@pm.me>
2023-11-08 11:37:33 -0800

    Moved over some functions and style options from app.R

--- R/utils.R
+++ R/utils.R
@@ -29,4 +29,13 @@ is_blank <- function(x) {
   x == ""
 }
 
+label_mandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+
+app_css <- ".mandatory_star { color: red; }"
+
 
