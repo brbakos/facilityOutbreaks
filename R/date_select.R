98f4b66e9e3750f369b3dc7342f27467aae29e5e
brbakos <bbakos@pm.me>
2023-11-08 11:37:09 -0800

    Modified to be a general function rather than a module

--- R/date_select.R
+++ R/date_select.R
@@ -1,10 +1,9 @@
-dateSelectUI <- function(id, label, value = lubridate::NA_Date_) {
-  dateInput(NS(id, "dateSelect"), label,
-            max = Sys.Date(),
-            value = value)
-}
-
-dateSelectServer <- function(id) {
-  moduleServer(id, function(input, output, session) {
-  })
+date_select <- function(id, label, value = lubridate::NA_Date_, ...) {
+  dateInput(
+    id,
+    label,
+    max = Sys.Date(),
+    value = value,
+    ...
+  )
 }
