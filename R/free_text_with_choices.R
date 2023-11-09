d8f3cf9356f89384a15d6c2ade7a9580b53f4ba7
brbakos <bbakos@pm.me>
2023-11-09 08:58:18 -0800

    Documentation

--- R/free_text_with_choices.R
+++ R/free_text_with_choices.R
@@ -1,7 +1,21 @@
-#' selectizeInput with some modifications
+#'  @title Select Input
 #'
-#' @details This is a wrapper for selectizeInput which sets some default parameters that are commonly used.
+#'  @description
+#'  selectizeInput with some modifications. A simple wrapper for `shinydashboard::selectizeInput()`
+#'
+#'
+#'  @param id The id to refer to the element on the server side
+#'  @param label What the question will be on the form
+#'  @param choices What values will be seen by the user in the drop down menu
+#'  @param ... Other optional arguments supplied to `options`
+#'
+#'  @returns Some HTML that produces a dropdown for users to select options.
+#'
+#'  @details This is a wrapper for selectizeInput which sets some default parameters that are commonly used.
 #'  Users need to be able to create new entries, and we want the default start up to always be blank.
+#'
+#'  @seealso \code{\link[shinydashboard]{selectizeInput}}
+#'
 selectize_input <- function(id, label, choices = NULL, ...) {
   selectizeInput(
     id,
